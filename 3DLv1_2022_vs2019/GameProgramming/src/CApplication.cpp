323881604dfe68a26c0dcd8df1039152fd7c9dae
Ikenaga-Hiroki-Fukuyama <gc22002@anabuki-net.ne.jp>
2022-09-27 12:11:03 +0900

    課題3完了

--- 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
@@ -3,6 +3,7 @@
 #include"glut.h"
 #include "CRectangle.h"
 #include"CVector.h"
+#include"CTriangle.h"
 
 //クラスのstatic変数
 CTexture CApplication::mTexture;
@@ -87,6 +88,30 @@ void CApplication::Update()
 	//glVertex3f(0.0f, 0.0f, 0.0f);
 	//glVertex3f(0.0f, 0.0f, 1.0f);
 
+	//三角形クラスのインスタンス作成
+	CTriangle t0;
+	//法線と頂点の設定
+	t0.Vertex(CVector(1.0f, 0.0f, 0.5f), CVector(2.0f, 0.0f, 0.0f), CVector(1.0f, 0.0f, -0.5f));
+	t0.Normal(CVector(0.0f, 1.0f, 0.0f));
+	//三角形の描画
+	t0.Render();
+
+	//三角形クラスのインスタンス作成
+	CTriangle t1;
+	//法線と頂点の設定
+	t1.Vertex(CVector(0.5f, 1.0f, 0.0f), CVector(0.0f, 2.0f, 0.0f), CVector(-0.5f, 1.0f, 0.0f));
+	t1.Normal(CVector(1.0f, 0.0f, 0.0f));
+	//三角形の描画
+	t1.Render();
+
+	//三角形クラスのインスタンス作成
+	CTriangle t2;
+	//法線と頂点の設定
+	t2.Vertex(CVector(0.0f, 0.0f, 2.0f), CVector(0.0f, -0.5f, 1.0f), CVector(0.0f, 0.5f, 1.0f));
+	t2.Normal(CVector(1.0f, 0.0f, 0.0f));
+	//三角形の描画
+	t2.Render();
+
 	//描画終了
-	glEnd();
+	//glEnd();
 }
