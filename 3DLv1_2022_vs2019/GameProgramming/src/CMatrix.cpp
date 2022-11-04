94f0456ac00f2529a2ff095c720f635addca7dc1
Ikenaga-Hiroki-Fukuyama <gc22002@anabuki-net.ne.jp>
2022-11-04 12:01:07 +0900

    課題14完了

--- 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
@@ -6,6 +6,23 @@
 //数学関数のインクルード
 #include <math.h>
 
+void CMatrix::M(int row, int col, float value)
+{
+	mM[row][col] = value;
+}
+
+//移動行列の作成
+//Translate(移動量X,移動量Y,移動量Z)
+CMatrix CMatrix::Translate(float mx, float my, float mz)
+{
+	mM[0][0] = 1.0f, mM[0][1] = 0.0f, mM[0][2] = 0.0f, mM[0][3] = 0.0f;
+	mM[1][0] = 0.0f, mM[1][1] = 1.0f, mM[1][2] = 0.0f, mM[1][3] = 0.0f;
+	mM[2][0] = 0.0f, mM[2][1] = 0.0f, mM[2][2] = 1.0f, mM[2][3] = 0.0f;
+	mM[3][0] = mx, mM[3][1] = my, mM[3][2] = mz, mM[3][3] = 1.0f;
+	//この行列を返す
+	return *this;
+}
+
 //回転行数(X軸)の作成
 //RotateX(角度)
 CMatrix CMatrix::RotateX(float degree)
