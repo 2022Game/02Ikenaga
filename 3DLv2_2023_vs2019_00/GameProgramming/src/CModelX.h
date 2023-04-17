d70d7402e6c8d1123005dccf109d4320755a3734
Ikenaga-Hiroki-Fukuyama <gc22002@anabuki-net.ne.jp>
2023-04-17 15:39:03 +0900

    課題2謎

--- 3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
@@ -16,8 +16,12 @@ public:
 	//ファイル読み込み
 	void Load(char* file);
 private:
+	//単語の取り出し
+	char* GetToken();
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
 	char* mpPointer;     //読み込み位置
-	char* mToken[1024];  //取り出した単語の領域
+	char mToken[1024];  //取り出した単語の領域
 };
 
 #endif
