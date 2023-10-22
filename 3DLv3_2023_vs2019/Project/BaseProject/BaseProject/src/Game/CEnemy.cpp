34ebb66ca5740a6fa86ded6acf1a13bca096e801
Ikenaga-Hiroki-Fukuyama <gc22002@anabuki-net.ne.jp>
2023-10-22 23:20:23 +0900

    アニメーション

--- 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/CEnemy.cpp
+++ 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/CEnemy.cpp
@@ -18,7 +18,18 @@ const CEnemy::AnimData CEnemy::ANIM_DATA[] =
 	//{ "Character\\Slime\\animation\\SlimeAttack2.x",	true,	26.0f	},  //攻撃2
 	//{ "Character\\Slime\\animation\\SlimeDie.x",	true,	81.0f	},  //死ぬ 41.0f
 	//{ "Character\\Slime\\animation\\SlimeDizzy.x",	true,	100.0f	},  //めまい 41.0f
-	{ "Character\\Slime\\animation\\SlimeGetHit.x",	true,	100.0f	},  //ヒット 26.0f
+	//{ "Character\\Slime\\animation\\SlimeGetHit.x",	true,	100.0f	},  //ヒット 26.0f
+	//{ "Character\\Slime\\animation\\SlimeIdleBattle.x",	true,	50.0f	},  //アイドルバトル 25.0f
+	//{ "Character\\Slime\\animation\\SlimeIdleNormal.x",	true,	51.0f	},  //アイドル通常
+	//{ "Character\\Slime\\animation\\SlimeRun.x",	true,	21.0f	},  //走る
+	//{ "Character\\Slime\\animation\\SlimeSenseSomethingRoutine.x",	true,	71.0f	},  //見回す
+	//{ "Character\\Slime\\animation\\SlimeSenseSomethingStart.x",	true,	63.0f	},  //開始の見回す
+	//{ "Character\\Slime\\animation\\SlimeTaunt.x",	true,	21.0f	},  //挑発
+	//{ "Character\\Slime\\animation\\SlimeVictory.x",	true,	81.0f	},  //勝利
+	//{ "Character\\Slime\\animation\\SlimeWalk.x",	true,	31.0f	},  //歩く
+	//{ "Character\\Slime\\animation\\SlimeWalkBack.x",	true,	31.0f	},  //後ろに歩く
+    //{ "Character\\Slime\\animation\\SlimeWalkLeft.x",	true,	31.0f	},  //左に移動
+	{ "Character\\Slime\\animation\\SlimeWalkRight.x",	true,	31.0f	},  //右に移動
 };
 
 // コンストラクタ
